529e96416b90b9460889bef998d527048090eb71
leosa <leosa@DESKTOP-SGITTK5>
2019-05-22 13:34:21 +0200

    Updated work

---
+++ view/src/main/java/view/Screen.java
@@ -0,0 +1,37 @@
+/**
+ * @author Léo Sanchez leo.sanchez@viacesi.fr
+ * @version 1.0
+ */
+package view;
+import java.awt.*;
+import javax.swing.*;
+ 
+public class Screen extends JPanel {
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(1000, 1000);
+	}
+	
+	protected void paintComponent(Graphics g) {
+
+		int xStart=1000/16;
+		int yStart=1000/16;
+		
+		g.setColor( Color.red );
+		
+		while(xStart < 1000) {
+		g.drawLine ( xStart, 0, xStart, 1000 );
+		xStart += 1000/16;
+		}
+		g.setColor( Color.green );
+		while(yStart < 1000) {
+			g.drawLine ( 0, yStart, 1000, yStart );
+			yStart += 1000/16;
+			}
+ 
+	}
+	public Screen()
+	{
+		
+	}
+}
---
+++ view/src/main/java/view/Window.java
@@ -0,0 +1,35 @@
+/**
+ * @author Léo Sanchez leo.sanchez@viacesi.fr
+ * @version 1.0
+ */
+package view;
+import view.Screen;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.Color;
+public class Window {
+	private JFrame window;
+	public Window(String text)
+	{
+		window = new JFrame();
+		window.setSize(1000, 1000);
+		window.setTitle(text);
+		window.setLocation(400, 10);
+		window.pack();
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(1000, 1000);
+	}
+	
+	public void addScreen(Screen screen) {
+		screen.setPreferredSize(new Dimension(1000, 1000));
+		screen.setBackground(Color.BLACK);
+		window.setContentPane(screen);
+	}
+	
+}

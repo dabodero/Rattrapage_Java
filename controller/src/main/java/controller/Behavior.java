8ad2b7dac114ca93d4eb7a49531ea09bc93ddbf5
bolex <biblex1222@gmail.com>
2019-05-22 14:52:03 +0200

    prototyp right, left, up, down

---
+++ controller/src/main/java/controller/Behavior.java
@@ -0,0 +1,33 @@
+package controller;
+
+public class Behavior {
+
+        /**
+         * The update method, update the position of the sprites.
+         * @version 1.0
+         */
+        protected void update() {}
+        protected int type_up; // Indicates of what is composed the up block
+        protected int type_down;	// Same
+        protected int type_left; //Same
+        protected int type_right; //Same
+        protected String spritePath; //Same
+
+
+
+        protected void GoUp(){
+
+        }  // method to move element above
+
+        protected void GoDown(){
+
+        }	// method to move element below
+
+        protected void GoRight(){
+
+        }	// method to move element on right
+
+        protected void GoLeft(){
+
+        }	// method to move element on left
+}

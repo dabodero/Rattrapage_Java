fcd1621044beea0a0187514f8d767f17c40c1240
leosa <leosa@DESKTOP-SGITTK5>
2019-05-22 13:30:54 +0200

    Change of main.

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -1,18 +1,15 @@
 /**
- * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @author Léo Sanchez leo.sanchez@viacesi.fr
  * @version 1.0
  */
 package main;
-
-import contract.ControllerOrder;
-import controller.Controller;
-import model.Model;
-import view.View;
+import view.Window;
+import view.Screen;
 
 /**
  * The Class Main.
  *
- * @author Jean-Aymeric Diet
+ * @author Léo Sanchez
  */
 public abstract class Main {
 
@@ -23,12 +20,8 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
-        final Model model = new Model();
-        final View view = new View(model);
-        final Controller controller = new Controller(view, model);
-        view.setController(controller);
-
-        controller.control();
-        controller.orderPerform(ControllerOrder.English);
+     Window fenetre = new Window("Boulder Dash");
+     Screen screen = new Screen();
+     fenetre.addScreen(screen);
     }
 }

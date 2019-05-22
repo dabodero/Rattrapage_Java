7c79503e8b8b82088c31f45b5b0d4f1195118c70
leosa <leosa@DESKTOP-SGITTK5>
2019-05-22 14:00:01 +0200

    Updated work

--- controller/src/main/java/controller/behavior.java
+++ controller/src/main/java/controller/behavior.java
@@ -1,4 +1,18 @@
+/**
+ * @author Basile Lecouturier 
+ * @version 1.0
+ */
 package controller;
 
-public class behavior {
+public abstract class behavior {
+	/**
+     * The update method, update the position of the sprites.
+     * @version 1.0     
+     */
+	protected void update();
+	protected int type_haut; // Indicates of what is composed the up block
+	protected int type_bas;	// Same
+	protected int type_gauche; //Same
+	protected int type_droite; //Same
+	protected String spritePath; //Same
 }

f9ac0fd8ed953556238bcc5f027dcd4b1ae06ddc
bolex <biblex1222@gmail.com>
2019-05-26 17:47:59 +0200

    class hero

--- model/src/main/java/model/Hero.java
+++ model/src/main/java/model/Hero.java
@@ -3,10 +3,23 @@ package model;
 public class Hero extends character{
 
 
-    public Hero() {
-        X = 1;
-        Y = 1;
-        alive = true;
+    public Hero(){
+        super();
     }
 
+   /* essai de ingletonprivate Hero(){
+    }
+
+    private static class SingletonHolder{
+
+        private final static Hero instance = new Hero();
+    }
+
+    public static Hero getInstance(){
+
+        return SingletonHolder.instance;
+    }*/
+
 }
+
+

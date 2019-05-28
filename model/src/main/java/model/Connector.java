b8430213768cc72a831f544284b936c72990933a
loisc <loisc@LOIS-PC>
2019-05-28 21:28:47 +0200

    Updated Work fix little bug

--- model/src/main/java/model/Connector.java
+++ model/src/main/java/model/Connector.java
@@ -1,5 +1,127 @@
 package model;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import controller.Behavior;
+import view.Screen;
+
 public class Connector {
 
-}
+/**
+*     Variable for position
+*/	
+	
+	static int  X = 0;
+	static int  Y= 0;
+	
+/**
+*	identifiers for connection to DB
+*/  
+
+	final static String url = "jdbc:mysql://localhost:3306/jpublankproject";
+    final static String user = "root";
+    final static String passwd = "";
+    
+    
+
+	public void run(Screen screen_) {
+
+    	
+        
+        /*
+        *Try to connect to the database
+        */
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            java.sql.Connection conn = DriverManager.getConnection(url, user, passwd);
+         
+      //Creation of Statement object
+      Statement state = conn.createStatement();
+      //The ResultSet object contains the result of the SQL query
+      ResultSet result = state.executeQuery("CALL Show_Map_One"); //Change for the differents maps
+      //metadata recoveries
+      java.sql.ResultSetMetaData resultMeta = result.getMetaData();
+      
+      
+      ArrayList<Behavior> map = new ArrayList<Behavior>();
+     
+      //assigning the position of each character in the table   
+     String res = null ;
+      while(result.next()){         
+      for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+      res = result.getObject(i).toString();
+      for(int position = 1; position <=256; position++) {
+       
+    	  if(Y<16) {
+    		      		  
+    		  Y++;
+    	  }
+    	  else {
+    		 X++;
+    		  Y=1;
+    	  }
+    	  	
+    	 
+    	 
+   switch (res.charAt(position)) {
+   
+   		case 'D' : 
+   			Dirt dirt = new Dirt("C:\\\\Users\\\\loisc\\\\Desktop\\\\PROJET\\\\GIT\\\\boulder_dash\\\\games_pictures\\\\image\\\\unbreak_dirt.png",X,Y); //New Object (path,X,Y) 
+   			
+   				
+	  map.add (dirt); // method to add a sprite and his behavior in the case
+	   
+	   break;
+      
+        case 'W' :
+        	Solid wall = new Solid("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\solid.png",X,Y);  //New Object (path,X,Y)   	   
+        	map.add (wall); // method to add a sprite and his behavior in the case
+        	break;
+        case 'J' :
+        	Diamond diamond = new Diamond("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\Diamand.png",X,Y);  //New Object (path,X,Y)    	   
+        	map.add (diamond);// method to add a sprite and his behavior in the case
+        	break;  
+        case 'R' :
+        	Rock rock = new Rock("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\rock.png",X,Y);    //New Object (path,X,Y)  	   
+        	map.add (rock);// method to add a sprite and his behavior in the case
+        	break; 
+        	
+        case 'A' :
+        	Air air = new Air("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\broken_wall.png",X,Y);  //New Object (path,X,Y)    	   
+        	map.add (air);	// method to add a sprite and his behavior in the case   
+        	break;
+        
+        case 'E' :
+          Exit exit = new Exit("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\door.png",X,Y);  //New Object (path,X,Y)       
+          map.add (exit); // method to add a sprite and his behavior in the case
+          break;
+        case 'H' :
+          Hero hero = new Hero("C:\\\\\\\\Users\\\\\\\\loisc\\\\\\\\Desktop\\\\\\\\PROJET\\\\\\\\GIT\\\\\\\\boulder_dash\\\\\\\\games_pictures\\\\\\\\image\\\\\\\\\\\\Face1.png",X,Y);  //New Object (path,X,Y)       
+          map.add (hero); // method to add a sprite and his behavior in the case
+          break;   
+   
+        }
+        }
+      }
+      
+      screen_.setMap(map);
+      
+//release memory
+      result.close(); 
+      state.close();
+            
+    } catch (Exception e) {
+      e.printStackTrace();
+    } ;
+        
+    
+        
+        
+        
+	 
+
+}}
+ 
--- model/src/test/java/model/ConnectorTest.java
+++ model/src/test/java/model/ConnectorTest.java
@@ -1,14 +1,17 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import controller.BehaviorSpritePath;
 
-public class ConnectorTest {
+
+public class ConnectorTest  {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,6 +23,7 @@ public class ConnectorTest {
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
@@ -27,8 +31,70 @@ public class ConnectorTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+  /*
+* Test to check if the identifiers are good
+*/
+	public void testConnectionDB() {
+		
+		// declaration of identifiers
+        String good_url = "jdbc:mysql://localhost:3306/jpublankproject";
+        String good_user = "root";
+        String good_passwd ="";
+    //comparison between the received identifiers and those declared above               
+        assertEquals(Connector.passwd,good_passwd);
+        if(Connector.passwd != good_passwd) {
+        fail("False Password");
+        }
+        assertEquals(Connector.url,good_url);
+        if(Connector.url != good_url) {
+        fail("False url");
+        }
+        assertEquals(Connector.user,good_user);
+        if(Connector.user != good_user) {
+        fail("False user");
+        }
+		
 	}
-
+/*	@Test */
+  /*
+* Test to check if sprite paths are correct
+*/
+	/*public void testPathSprite() {
+		
+		 String good_unbreak_dirt = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\Mur\\\\unbreak_dirt.png";
+		 String good_solid = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\Mur\\\\unbreakable_wall.png";
+		 String good_broken_wall = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\Mur\\\\broken_wall.png";
+		 String good_Diamond = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\Diamand\\\\Diamand.png";
+		 String good_rock = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\rock\\\\rock.png";
+		 String good_hero = "C:\\\\Users\\\\TRAVAIL\\\\mes_projets\\\\projet_java\\\\boulder_dash\\\\games_pictures\\\\Personnage\\\\Face\\\\Face1.png";
+		
+		 
+		 //passer par une classe temporaire pour les variables (pas possibles de faire Static string là ou il est de base)
+		 assertEquals(BehaviorSpritePath.unbreak_dirt,good_unbreak_dirt);
+	        if(BehaviorSpritePath.unbreak_dirt != good_unbreak_dirt) {
+	        fail("Path of unbreak_dirt is false");
+	        }
+	        assertEquals(BehaviorSpritePath.solid,good_solid);
+	        if(BehaviorSpritePath.solid != good_solid) {
+	        fail("Path of solid is false");
+	        }
+	        assertEquals(BehaviorSpritePath.broken_wall,good_broken_wall);
+	        if(BehaviorSpritePath.broken_wall != good_broken_wall) {
+	        fail("Path of broken_wall is false");
+	        }
+	        assertEquals(BehaviorSpritePath.Diamond,good_Diamond);
+	        if(BehaviorSpritePath.Diamond != good_Diamond) {
+	        fail("Path of Diamond is false");
+	        }
+	        assertEquals(BehaviorSpritePath.rock,good_rock);
+	        if(BehaviorSpritePath.rock != good_rock) {
+	        fail("Path of Rock is false");
+	        }
+	        assertEquals(BehaviorSpritePath.hero,good_hero);
+	        if(BehaviorSpritePath.hero != good_hero) {
+	        fail("Path of hero is false");
+	        }
+		 
+	}
+*/
 }

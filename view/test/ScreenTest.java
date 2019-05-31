3e3ad2306592429022b4892765a6197a320e39a5
loisc <loisc@LOIS-PC>
2019-05-31 16:41:56 +0200

    Junit Test

---
+++ view/test/ScreenTest.java
@@ -0,0 +1,46 @@
+
+
+/*import static org.junit.Assert.*;
+
+
+import javax.swing.JFrame;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import view.Window;
+
+
+public class ScreenTest {
+	String expected_title = "Boulder Dash";
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		Window window = new Window("yotlm");
+		
+		}
+	
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testWindows() {
+		
+		
+		assertTrue("hello",(expected_title == Window.text));
+	}
+
+}*/
